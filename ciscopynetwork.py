cd8e731642f0eff1b906621f90a288766cfcbff9
John Natschev <jnatschev@optusnet.com.au>
2017-10-24 18:38:33 +1100

    fix reachable method
    fixed the reachable method.

--- ciscopynetwork.py
+++ ciscopynetwork.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 '''
 This purpose of this module is to provide network related methods and
-attributes associated with retrieving, storing, and processing OB
-Configuration Management Database (CMDB) Configuration Item (CI)
-elements.
+attributes associated with retrieving, storing, and processing network devices.
 '''
-from pyping import ping as pping
+#from pyping import ping as pping
+from subprocess import run as sp_run, PIPE as sp_PIPE
 
 class CiscoPyNetwork(object):
-    def __init__(self, ipaddress, timeout=3000, count=2, packet_size=55):
+    def __init__(self, ipaddress, wait_time='3000', count='2', packet_size='55'):
         self.ipaddress = ipaddress
-        self.timeout = timeout
+        self.wait_time = wait_time
         self.count = count
         self.packet_size = packet_size
 
     @property
-    def reachable(self):
-        result = False
-        try:
-            pping_result = pping(self.ipaddress,
-                                 timeout=self.timeout,
-                                 count=self.count,
-                                 packet_size=self.packet_size)
-            if pping_result.ret_code == 0:
-                result = True
-        except:
-            pass
+    def is_reachable(self):
+        '''
+        Is the network device reachable. Basically a ping test.
         
+        The ping command assumes that this method will executed on a
+        Linux/Unix type computer.
+        '''
+        result = False
+        ping_cmd = ['ping', '-c', self.count, '-W', self.wait_time, '-s',
+                    self.packet_size, self.ipaddress]
+        spr_cp = sp_run(ping_cmd, stdout=sp_PIPE,
+                        stderr=sp_PIPE)
+        
+        if spr_cp.returncode == 0:
+            result = True
+
         return result
     
     def __repr__(self):
