437a9fb3b15a9ca286e28a6f8eb341d3095a9941
John Natschev <jnatschev@optusnet.com.au>
2018-07-01 19:37:21 +1000

    use ipaddress instead of netaddr
    decided to use ipaddress module instead of the netaddr module.
    
    added testing for the value of ip_address/subnetmask and whether other keyword parameters of the correct type: bool.

--- ciscopyinterface.py
+++ ciscopyinterface.py
@@ -8,44 +8,53 @@ The class CiscoIPv4Interface defined in this module inherits the
 ipaddress.IPv4Interface class.
 """
 
-import netaddr
+import ipaddress
+import re
 
 
-class CiscoPyInterface(object):
-    def __init__(self, if_name, **kwargs):
+class CiscoPyInterfaces(list):
+    pass
+
+
+class CiscoPyIPv4Interface(ipaddress.IPv4Interface):
+    def __init__(self, if_name, ipv4, **kwargs):
         """
-
-        :param if_name:         Interface name (or designation)
-        :param bandwidth:       Interface bandwidth as a string
-        :param ipv4network:     Interface ip_address/subnet_mask as a string.
-                                The value of subnet_mask may be dotted
-                                notation or CIDR notation.
-        :param description:     Interface description
+        :param if_name:         interface name (or designation) as a string
+        :param ipv4:            ipv4_address/ipv4_subnetmask as a string
+        :param bandwidth:       interface bandwidth as a string
+        :param description:     interface description as a string
         :param ip_redirects:    True/False
         :param ip_unreachables: True/False
         :param ip_proxyarp:     True/False
         """
-        self.name = if_name
-        self.description = kwargs.get('description', '')
-        if kwargs.get('ipaddr_netmask') is not None:
-            self.ipv4network = netaddr.IPNetwork(kwargs.get('ipaddr_netmask'))
 
-    def mgtip_is_natted(self, l):
-        """
-        This method checks whether the OB TAC Management IP
-        address is natted. This method depends on the method named
-        ipadentaddr_to_ipaddress to set the attribute named dev_ips.
+        if '/' not in ipv4:
+            raise ValueError('parameter ipv4 not in correct format')
 
-        The attribute dev_ips is a list of class IPv4Address ip
-        addresses.
-        """
+        ip_regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+        if ip_regex.match(ipv4.split('/')[0]) is None:
+            raise ValueError('the ip address of paramter ipv4 is not in dotted '
+                             'notation.')
 
-        self.ipadentaddr_to_ipaddress(l)
-          
-        if len(self.dev_ips) != 0:
-            if isinstance(self.dev_ips, list):
-                if all(isinstance(v, IPv4Address) for v in self.dev_ips):
-                    if self.ip in self.dev_ips:
-                        self.mgtip_is_natted = False
-                    else:
-                        self.mgtip_is_natted = True
+        if ip_regex.match(ipv4.split('/')[-1]) is None:
+            raise ValueError('the subnet mask of paramter ipv4 is not in '
+                             'dotted notation.')
+
+        super(CiscoPyIPv4Interface, self).__init__(ipv4)
+
+        self.interface_name = if_name
+        self.bandwidth = kwargs.get('bandwidth')
+        self.description = kwargs.get('description')
+
+        if not isinstance(kwargs.get('ip_redirects'), bool):
+            raise ValueError('ip_redirects parameter is not type bool')
+        self.ip_redirects = kwargs.get('ip_redirects', False)
+
+        if not isinstance(kwargs.get('ip_unreachables'), bool):
+            raise ValueError('ip_unreachables parameter is not type bool')
+        self.ip_unreachables = kwargs.get('ip_unreachables', False)
+
+        if not isinstance(kwargs.get('ip_proxyarp'), bool):
+            raise ValueError('ip_proxyarp parameter is not type bool')
+        self.ip_proxyarp = kwargs.get('ip_proxyarp', False)
+
